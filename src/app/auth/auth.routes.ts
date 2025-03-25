e1c2e52534f5dfae97d43c7146b432ac7fe57b12
lgcholoquinga <luisg.choloquinga@gamil.com>
2025-03-24 21:15:16 -0500

    Estructura de carpetas por modulos

---
+++ src/app/auth/auth.routes.ts
@@ -0,0 +1,29 @@
+import { Routes } from '@angular/router';
+
+export default [
+	{
+		path: '',
+		loadComponent: () => import('./layout/auth-layout.component'),
+		children: [
+			{
+				path: 'login',
+				title: 'Login',
+				loadComponent: () => import('./pages/login/login.component'),
+			},
+			{
+				path: 'register',
+				title: 'Register',
+				loadComponent: () => import('./pages/register/register.component'),
+			},
+			{
+				path: '',
+				redirectTo: 'login',
+				pathMatch: 'full',
+			},
+			{
+				path: '**',
+				loadComponent: () => import('../common/pages/not-found/not-found.component'),
+			},
+		],
+	},
+] as Routes;
---
+++ src/app/auth/layout/auth-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+	selector: 'amc-auth-layout',
+	imports: [RouterOutlet],
+	templateUrl: './auth-layout.component.html',
+	styleUrl: './auth-layout.component.scss',
+})
+export default class AuthLayoutComponent {}
